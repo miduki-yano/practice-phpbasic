79dd44c882eaab3db2e30c0d30e62cfe8a705f40
miduki-yano <72035301+miduki-yano@users.noreply.github.com>
2024-01-25 17:10:44 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+#pythonファイルの拡張子は.py
+#----python3で変数名に使えない予約語----------------------------
+['False', 'None', 'True', 'and', 'as', 'assert', 'break',
+  'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 
+  'finally', 'for', 'from', 'global', 'if', 'import', 'in', 
+  'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+  'return', 'try', 'while', 'with', 'yield']
+#--------------------------------------------------------------
+
+lang = "Diego" #変数に文字列を代入(pythonには型宣言がない)
+lang2 = "diego"
+
+print(lang)
+print(lang2)
